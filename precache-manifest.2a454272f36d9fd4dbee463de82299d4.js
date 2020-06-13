70156776d012453277d2794b46e12ee404dcd9de
Rajesh <rajeshkumarveer2305@gmail.com>
2020-06-13 11:41:55 +0530

    Updates

---
+++ precache-manifest.2a454272f36d9fd4dbee463de82299d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a54f38587807ab804a14d3f41c4323b",
+    "url": "/newudemy/index.html"
+  },
+  {
+    "revision": "42beff709134082a0061",
+    "url": "/newudemy/static/css/main.bf6eb4e8.chunk.css"
+  },
+  {
+    "revision": "9d7b86ba2cb9e7f55934",
+    "url": "/newudemy/static/js/2.57ba3fe6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newudemy/static/js/2.57ba3fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42beff709134082a0061",
+    "url": "/newudemy/static/js/main.95fbe7a1.chunk.js"
+  },
+  {
+    "revision": "20f81a72ff7c4a86f799",
+    "url": "/newudemy/static/js/runtime-main.2d53c390.js"
+  },
+  {
+    "revision": "7dc11bef50f465abd9c5964ddac72579",
+    "url": "/newudemy/static/media/udemy_home.7dc11bef.jpg"
+  }
+]);
